838aee546232fcb48c6176f359abe82772b49ca0
Onkar Shelar <112194806+OnkarShelarr@users.noreply.github.com>
2024-12-26 22:06:46 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,27 +1,51 @@
-const express = require("express");
-const cors = require("cors");
-const mysql = require("mysql2");
+const express = require('express');   
+const cors = require('cors');
+const mysql = require('mysql2');
 
 const app = express();
-app.use(cors());
 app.use(express.json());
+app.use(cors());
 
 const con = mysql.createConnection({
     host: "sql12.freesqldatabase.com",
-    user: "sql12754094",
-    password: "Wcgiqlugf8",
-    database: "sql12754094"
+    user: "sql12754302",
+    password: "6FUY4GkUia",
+    database: "sql12754302"
 });
 
-app.post("/save", (req, res) => {
-    let sql = "insert into student values (?, ?)";
-    let data = [req.body.name, req.body.feedback];
+app.post("/ss", (req, res) => {
+    let sql = "insert into student values(?, ?, ?)";
+    let data = [req.body.rno, req.body.name, req.body.marks];
     con.query(sql, data, (err, result) => {
-        if (err)    res.send(err);
-         
-        else        res.send("result");
-        
+        if (err)    res.send(err);   
+        else        res.send(result);
     });
 });
 
-app.listen(9000, () => {    console.log("ready to serve @ 9000");});
+app.get("/gs", (req, res) => {
+    let sql = "select * from student";
+    con.query(sql, (err, result) => {
+        if (err)    res.send(err);   
+        else        res.send(result);
+    });
+});
+
+app.put("/us", (req, res) => {
+    let sql = "update student set name = ?, marks = ? where rno = ?";
+    let data = [req.body.name, req.body.marks, req.body.rno];
+    con.query(sql, data, (err, result) => {
+        if (err)    res.send(err);   
+        else        res.send(result);
+    });
+});
+
+app.delete("/ds", (req, res) => {
+    let sql = "delete from student where rno = ?";
+    let data = [req.body.r];
+    con.query(sql, data, (err, result) => {
+        if (err)    res.send(err);   
+        else        res.send(result);
+    });
+});
+
+app.listen(9000, () => { console.log("ready to serve @ 9000"); });
